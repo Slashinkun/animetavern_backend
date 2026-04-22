ad398e98f6d62b15cec6510d936860a54bcb0599
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-04-22 22:59:24 +0200

    ajout precision methodes routes

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"main/middleware"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -20,15 +21,33 @@ func main() {
 		println("Connecté à la DB")
 	}
 
-	// routes
-	http.HandleFunc("/anime/", middleware.OptionalMiddleware(handlers.GetAnimePage))
+	router := mux.NewRouter()
 
-	http.HandleFunc("/register", handlers.RegisterHandler)
-	http.HandleFunc("/login", handlers.LoginHandler)
-	http.HandleFunc("/logout", handlers.LogoutHandler)
-	http.HandleFunc("/search", handlers.Search)
+	router.Use(middleware.CORSMiddleware)
+
+	// routes
+
+	router.HandleFunc("/register", handlers.RegisterHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST", "OPTIONS")
+	router.HandleFunc("/search", handlers.Search).Methods("GET")
+
+	//anime
+	router.HandleFunc(
+		"/anime/{anime_id}",
+		middleware.OptionalMiddleware(handlers.GetAnimePage),
+	).Methods("GET", "OPTIONS")
+
+	//user
+	router.HandleFunc(
+		"/user/{id}",
+		middleware.OptionalMiddleware(handlers.UserPageHandler),
+	).Methods("GET", "OPTIONS")
+
+	//auth
+	router.HandleFunc("/me", handlers.MeHandler).Methods("GET", "OPTIONS")
 
 	fmt.Println("Serveur démarré sur le port 8080...")
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 }
